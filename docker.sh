c2f27d3a2a5385861532ac9625631df0734b29b2
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2024-10-21 14:52:49 +0700

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,6 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo docker run hello-world
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose                  
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
