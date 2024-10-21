8cc345a866ba9c9a9737b0b3a3af11a23a931407
kietcaodev <71913797+kietcaodev@users.noreply.github.com>
2024-10-21 14:24:17 +0700

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,23 @@
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose                  
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.3
+my_primary_ip=$(ip route get 8.8.8.8 | head -1 | grep -Po '(\d+\.){3}\d+' | tail -n1)
+echo "     Congratulations!"
+echo "     * Access Portainer UI:"
+echo "     https://$my_primary_ip:9443"
